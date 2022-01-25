648bf8f2103c475e3097b76bb0751b5b018e68d8
Diana2886 <77288519+Diana2886@users.noreply.github.com>
2022-01-25 13:38:02 +0600

    feat:
    add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  const openBrackets = [];
+  for (let brackets of bracketsConfig) {
+    openBrackets.push(brackets[0]);
+  }
+  const bracketPairs = Object.fromEntries(bracketsConfig.map(i => i.reverse()));
+  for (let i = 0; i < str.length; i++) {
+    let item = str[i];
+    let stackLastItem = stack[stack.length - 1];
+    if (openBrackets.includes(item)) {
+      if (stackLastItem === item && bracketPairs[item] === item) {
+        stack.pop();
+        continue;
+      } 
+      stack.push(item);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (bracketPairs[item] === stackLastItem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    console.log(stack);
+  }
+  return stack.length === 0;
 }
