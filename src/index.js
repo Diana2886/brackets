f82b146e91721d03b902e522fba8cd12c7e43575
diana2886 <tdm2886@gmail.com>
2022-01-28 17:03:38 +0600

    change solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketPairs = bracketsConfig.map(item => item.join(''));
+  for (let i = 0; i < bracketPairs.length;) {
+    if (str.includes(bracketPairs[i])) {
+      str = str.replace(bracketPairs[i], '');
+      i = 0;
+    } else i++;
+  }
+  return str.length === 0;
 }
